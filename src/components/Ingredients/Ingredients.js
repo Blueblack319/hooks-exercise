6df04c65f53938568c8b7ca86868bf399ca3fac0
Yang Jae Hoon <didwongs789@naver.com>
2020-09-30 13:12:36 +0900

    Sending Http Requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,14 +7,45 @@ import Search from "./Search";
 function Ingredients() {
   const [ingredients, setIngredients] = useState([]);
   const handleIngredientAdded = (ingredient) => {
-    setIngredients(
-      // (prevIngredients) => [
-      //   ...prevIngredients,
-      //   { id: Math.random.toString(), ...ingredient },
-      // ]
-      (prevIngredients) =>
-        prevIngredients.concat({ id: Math.random().toString(), ...ingredient })
-    );
+    fetch("https://hooks-exercise.firebaseio.com/ingredients.json", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ingredient),
+    })
+      .then((res) => res.json())
+      .then((ing) =>
+        setIngredients((prevIngredients) =>
+          prevIngredients.concat({ id: ing.name, ...ingredient })
+        )
+      );
+    // try {
+    //   const ingsRes = await fetch(
+    //     "https://hooks-exercise.firebaseio.com/ingredients.json",
+    //     {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify(ingredient),
+    //     }
+    //   );
+    //   const ings = await ingsRes.json();
+    //   setIngredients(
+    //     // (prevIngredients) => [
+    //     //   ...prevIngredients,
+    //     //   { id: Math.random.toString(), ...ingredient },
+    //     // ]
+    //     (prevIngredients) =>
+    //       prevIngredients.concat({
+    //         id: ings.name,
+    //         ...ingredient,
+    //       })
+    //   );
+    // } catch (err) {
+    //   console.log(err);
+    // }
   };
 
   const handleIngredientRemoved = (id) => {
